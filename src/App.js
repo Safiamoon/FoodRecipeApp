36d3a12f66d54198a8bdb3ee2c1fe83f08de6459
Safiamoon <msellek.safia@gmail.com>
2021-02-02 15:34:55 +0100

    display data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,61 @@
 import './App.css';
+import React, {useState} from 'react';
+import axios from 'axios';
+import Recipe from "./components/Recipe";
+import {v4 as uuidv4} from 'uuid';
+import Alert from "./components/Alert";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const [alert, setAlert] = useState("");
+
+  const APP_ID = "4e9f05eb";
+  const APP_KEY = "9b904d703fa0d46a88ce1ac63f29f498";
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const getData = async () => {
+    if (query !== "") {
+      const result = await axios.get(url);
+      if (!result.data.more) {
+        return setAlert("No food with such name !");
+      }
+      console.log(result);
+      setRecipes(result.data.hits);
+      setQuery("");
+      setAlert("");
+    } else {
+      setAlert("Please fill the form");
+    }
+  };
+
+  const onChange = e => setQuery(e.target.value);
+
+  const onSubmit = e => {
+    e.preventDefault();
+    getData();
+  };
+
   return (
     <div className="App">
-      <h1> <i class="fas fa-utensils"></i> Food Recipe</h1>
+      <h1> <i className="fas fa-utensils"></i> Food Recipe</h1>
+      <form onSubmit={onSubmit} className="search-form">
+        {alert !== "" && <Alert alert={alert} />}
+        <input
+          type="text"
+          name="query"
+          onChange={onChange}
+          value={query}
+          autoComplete="off"
+          placeholder="Search Food"
+        />
+        <input type="submit" value="Search" />
+      </form>
+      <div className="recipes">
+        {recipes !== [] &&
+          recipes.map(recipe => <Recipe key={uuidv4()} recipe={recipe} />)}
+      </div>
     </div>
   );
 }
---
+++ src/components/Recipe.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+import RecipeDetails from "./RecipeDetails";
+
+const Recipe = ({recipe}) => {
+    const [show, setShow] = useState(false);
+    const{label, image, url, ingredients} = recipe.recipe;
+
+    return (
+        <div className="recipe">
+            <img src={image} alt={label}/> 
+            <h3>{label}</h3>
+            <button onClick={() => setShow(!show)}>Ingredients</button>
+            {show && <RecipeDetails ingredients={ingredients} />}
+        </div>
+    )
+}
+
+export default Recipe
